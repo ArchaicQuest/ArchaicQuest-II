6d5915c11bc3bb9aa75b18810a4819bd2787ca8d
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2018-11-03 06:07:58 +0000

    change list of itemflags to itemFlag with flag enums

--- Core/Item/Item.cs
+++ Core/Item/Item.cs
@@ -24,26 +24,27 @@ namespace ArchaicQuestII.Core.Item
             Weapon,
         }
 
-
+        [Flags]
         public enum ItemFlags
         {
-            Antievil, //zap if align -350 & lower
-            Antigood, //zap if align +350 & lower
-            Antineutral, //zap if align -350 to +350
-            Bless, // +20% resist dam.
-            Container, // isContainer
-            Cursed,
-            Equipable, // can be equipped
-            Evil, //glow on det.evil
-            Glow, //lights area
-            Holy,
-            Hum,  //affect n/a
-            Invis, //invisible
-            Nodrop, // cannot drop
-            Nolocate,   //locate spell fails
-            Noremove, //cannot remove w/o remove curse
-            QuestItem,
-            Vampric, // Drains hp on hoit
+            None = 0,
+            Antievil = 1, //zap if align -350 & lower
+            Antigood = 2, //zap if align +350 & lower
+            Antineutral = 4, //zap if align -350 to +350
+            Bless = 8, // +20% resist dam.
+            Container = 16, // isContainer
+            Cursed = 32,
+            Equipable = 64, // can be equipped
+            Evil = 128, //glow on det.evil
+            Glow = 256, //lights area
+            Holy = 512,
+            Hum = 1024, //affect n/a
+            Invis = 2024, //invisible
+            Nodrop = 4096, // cannot drop
+            Nolocate = 8192,   //locate spell fails
+            Noremove = 16384, //cannot remove w/o remove curse
+            QuestItem = 32768,
+            Vampric = 65536, // Drains hp on hoit
         }
 
         public enum ItemLocation
@@ -162,7 +163,7 @@ namespace ArchaicQuestII.Core.Item
 
         public DamageTypes DamageType { get; set; }
         public ArmourTypes ArmourType { get; set; }
-        public List<ItemFlags> ItemFlag { get; set; }
+        public ItemFlags ItemFlag { get; set; }
         public ItemTypes ItemType { get; set; }
         public AttackTypes AttackType { get; set; }
         public EqSlot Slot { get; set; }
