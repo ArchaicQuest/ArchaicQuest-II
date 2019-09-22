e86a87c7dc45d376f8b54c8db5555ad805f69e79
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2019-09-22 22:00:28 +0100

    api to create accounts

---
+++ API/Account/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using ArchaicQuestII.Engine.Character.Model;
+using Microsoft.AspNetCore.Mvc;
+using ArchaicQuestII.Core.Events;
+using ArchaicQuestII.Engine.Account;
+ 
+namespace ArchaicQuestII.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        [HttpPost]
+        [Route("api/Account")]
+        public void Post([FromBody] Account account)
+        {
+
+
+            if (!ModelState.IsValid)
+            {
+                var exception = new Exception("Invalid Account details");
+                throw exception;
+            }
+
+
+            var data = new Account()
+            {
+                UserName = account.UserName,
+                Characters = new List<Player>(),
+                Credits = 0,
+                Email = account.Email,
+                EmailVerified = false,
+                Password = BCrypt.Net.BCrypt.HashPassword(account.Password), //BCrypt.Verify("my password", passwordHash);
+                Stats = new AccountStats(),
+            };
+
+
+            DB.Save(data, "Account");
+
+        }
+
+    }
+}
--- Engine/Account/Account.cs
+++ Engine/Account/Account.cs
@@ -8,7 +8,7 @@ namespace ArchaicQuestII.Engine.Account
     public class Account
     {
         public string Id { get; set; }
-        public string Name { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
         /// <summary>
