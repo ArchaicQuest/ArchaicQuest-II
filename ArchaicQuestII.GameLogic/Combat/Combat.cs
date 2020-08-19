df2707b39043bcdd0857d010d1f5d915e1026d16
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2020-08-19 23:03:21 +0100

    Combat WIP

--- ArchaicQuestII.GameLogic/Combat/Combat.cs
+++ ArchaicQuestII.GameLogic/Combat/Combat.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ArchaicQuestII.GameLogic.Character;
+using ArchaicQuestII.GameLogic.Character.Status;
 using ArchaicQuestII.GameLogic.Core;
 using ArchaicQuestII.GameLogic.Effect;
 using ArchaicQuestII.GameLogic.Item;
@@ -46,9 +47,105 @@ namespace ArchaicQuestII.GameLogic.Combat
             return player.Attributes.Attribute[EffectLocation.Dexterity] / 5 + player.Attributes.Attribute[EffectLocation.Strength] / 10;
         }
 
+        public int BlockPoints(Player player)
+        {
+            var offensePoints = player.Attributes.Attribute[EffectLocation.Strength] / 5
+                                + player.Attributes.Attribute[EffectLocation.Dexterity] / 10;
+                                
+            var blockSkill = 100; //weapon types are hardcoded so make hardcoded skills for weapon types
+
+            return offensePoints + offensePoints * blockSkill / 100;
+        }
+
+        public int ToBlockChance(Player player, Player target)
+        {
+            return BlockPoints(player) * 100 / (BlockPoints(player) + DefensivePoints(target));
+        }
+
         public int ToHitChance(Player player, Player target)
         {
             return OffensivePoints(player) * 100 / (OffensivePoints(player) + DefensivePoints(target));
+        }
+
+        public int DamageReduction(Player defender, int damage)
+        {
+            var ArRating = defender.ArmorRating.Armour + 1;
+            var armourReduction = ArRating / (double)damage;
+
+            if (armourReduction > 4)
+            {
+                armourReduction = 4;
+            }
+            else if (armourReduction <= 0)
+            {
+                armourReduction = 1;
+            }
+
+            return (int)armourReduction;
+        }
+
+        public int CalculateDamage(Player player, Player target)
+        {
+            var weapon = GetWeapon(player);
+            var damage = 0;
+
+            if (weapon != null)
+            {
+                damage = Dice.Roll(1, weapon.Damage.Minimum, weapon.Damage.Maximum);
+
+            }
+            else
+            {
+                //Hand to hand
+                damage = Dice.Roll(1, 1, 6);
+            }
+
+            // Enhanced Damage Skill check
+            // increase damage is player has enhanced damage skill
+            if (false)
+            {
+            }
+
+            // calculate damage reduction based on target armour
+             var armourReduction = DamageReduction(target, damage);
+
+             //refactor this shit
+             var strengthMod = 0.5 + player.Attributes.Attribute[EffectLocation.Strength] / (double)100;
+             var levelDif = player.Level - target.Level <= 0 ? 1 : player.Level - target.Level;
+             var levelMod = levelDif / 2 <= 0 ? 1 : levelDif / 2;
+             var enduranceMod = player.Attributes.Attribute[EffectLocation.Moves]/ (double)player.MaxAttributes.Attribute[EffectLocation.Moves];
+             var criticalHit = 0;
+            
+             if (target.Status == CharacterStatus.Status.Sleeping || target.Status == CharacterStatus.Status.Stunned || target.Status == CharacterStatus.Status.Resting)
+             {
+                 criticalHit = 2;
+             }
+
+
+             int totalDamage = (int)(damage * strengthMod * criticalHit * levelMod);
+
+             if (armourReduction > 0)
+             {
+                 totalDamage = totalDamage / armourReduction;
+             }
+
+             if (totalDamage <= 0)
+             {
+                 totalDamage = 1;
+             }
+
+
+             return totalDamage;
+        }
+
+        public Item.Item GetWeapon(Player player)
+        {
+            return player.Equipped.Wielded;
+        }
+
+        public bool IsCriticalHit()
+        {
+            return Dice.Roll(1, 1, 20) == 20;
         }
 
         public bool DoesHit(int chance)
@@ -74,20 +171,64 @@ namespace ArchaicQuestII.GameLogic.Combat
             }
 
             var chanceToHit = ToHitChance(player, target);
-
             var doesHit = DoesHit(chanceToHit);
 
             if (doesHit)
             {
-                // check for block / dodge
-                    // if block, show block message
-                    // if dodge, show dodge message
-                // check if critical hit, roll 1d20?
-                    // do damage
+                var weapon = GetWeapon(player);
+                var hasEvadedDamage = false;
+
+                // avoidance percentage can be improved by core skills 
+                // such as improved parry, acrobatic etc 
+                // instead of rolling a D10, roll a D6 for a close to 15% increase in chance
+
+                //10% chance to attempt a dodge
+                if(Dice.Roll(1,1,10) == 1)
+                {
+
+                }
+
+                //10% chance to parry
+                if (!hasEvadedDamage && Dice.Roll(1, 1, 10) == 2)
+                {
+
+                }
+
+                // Block
+                if (!hasEvadedDamage && Dice.Roll(1, 1, 10) == 3)
+                {
+                    var chanceToBlock = ToBlockChance(target, player);
+                    var doesBlock = DoesHit(chanceToBlock);
+
+                    if (doesBlock)
+                    {
+
+                    }
+                    else
+                    {
+                        // block fail
+                    }
+                }
+
+                //dodge
+                //parry
+
+                var damage = CalculateDamage(player, target);
+
+                if (IsCriticalHit())
+                {
+                    // double damage
+                    damage *= 2;
+                }
+
+
+
+                 
+                // do damage
                 // check if target is dead or alive
-                    // award xp if dead
-                        // create corpse container holding targets inventory and money
-                            // end combat
+                // award xp if dead
+                // create corpse container holding targets inventory and money
+                // end combat
             }
             else
             {
