0d74f1afd6dffd6a3a7611392513f643eb562363
LiamKenneth <liam.kenneth@hotmail.co.uk>
2021-02-21 08:42:10 +0000

    adding key file back

---
+++ ArchaicQuestII.GameLogic/Core/Keys.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArchaicQuestII.GameLogic.Core
+{
+    /// <summary>
+    /// Add secrets here
+    /// </summary>
+    public class Keys
+    {
+        /// <summary>
+        /// Sends a message to a discord channel when ever a player uses a channel
+        /// OOC / newbie / gossip
+        /// </summary>
+        public  string ChannelDiscordWebHookURL { get; set; }
+
+        /// <summary>
+        /// Sends a message to a discord channel when ever a player joins the game, levels up or dies
+        /// </summary>
+        public string EventsDiscordWebHookURL { get; set; }
+    }
+}
