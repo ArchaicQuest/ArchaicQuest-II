3a71ea009faee5df8e5766aeeac6a28b9cd990a3
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2022-03-17 08:07:10 +0000

    prod build fix

--- ArchaicQuestII.GameLogic.Tests/Commands/Commands.cs
+++ ArchaicQuestII.GameLogic.Tests/Commands/Commands.cs
@@ -4,6 +4,7 @@ using ArchaicQuestII.GameLogic.Character;
 using ArchaicQuestII.GameLogic.Character.Equipment;
 using ArchaicQuestII.GameLogic.Character.Help;
 using ArchaicQuestII.GameLogic.Character.MobFunctions;
+using ArchaicQuestII.GameLogic.Character.MobFunctions.Healer;
 using ArchaicQuestII.GameLogic.Combat;
 using ArchaicQuestII.GameLogic.Commands;
 using ArchaicQuestII.GameLogic.Commands.Communication;
@@ -51,6 +52,7 @@ namespace ArchaicQuestII.GameLogic.Tests.Commands
         private readonly Mock<ICooking> _cooking;
         private readonly Mock<IUtilSkills> _utilSkills;
         private readonly Mock<IPassiveSkills> _passiveSkills;
+        private readonly Mock<IHealer> _healer;
         public CommandsTests()
         {
             _movement = new Mock<IMovement>();
@@ -76,6 +78,7 @@ namespace ArchaicQuestII.GameLogic.Tests.Commands
             _cooking = new Mock<ICooking>();
             _utilSkills = new Mock<IUtilSkills>();
             _passiveSkills = new Mock<IPassiveSkills>();
+            _healer = new Mock<IHealer>();
 
             _player.ConnectionId = "1";
             _player.Name = "Bob";
@@ -99,7 +102,7 @@ namespace ArchaicQuestII.GameLogic.Tests.Commands
                 }
             };
 
-            _commands = new GameLogic.Commands.Commands(_movement.Object, _roomActions.Object, _debug.Object, _skill.Object, _spell.Object, _object.Object, _inventory.Object, _communication.Object, _equipment.Object, _score.Object, _combat.Object, _cache.Object, _socials.Object, _commandHandler.Object, _core.Object, _mobFunctions.Object, _help.Object, _mobScripts.Object, _crafting.Object, _cooking.Object, _utilSkills.Object, _passiveSkills.Object);
+            _commands = new GameLogic.Commands.Commands(_movement.Object, _roomActions.Object, _debug.Object, _skill.Object, _spell.Object, _object.Object, _inventory.Object, _communication.Object, _equipment.Object, _score.Object, _combat.Object, _cache.Object, _socials.Object, _commandHandler.Object, _core.Object, _mobFunctions.Object, _help.Object, _mobScripts.Object, _crafting.Object, _cooking.Object, _utilSkills.Object, _passiveSkills.Object, _healer.Object);
 
         }
 
