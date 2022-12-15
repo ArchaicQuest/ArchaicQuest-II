83e40f6068056b9c1b6d419d8066521bc9802fe0
LiamKenneth <liam.kenneth@hotmail.co.uk>
2022-12-15 22:53:52 +0000

    Save command

---
+++ ArchaicQuestII.GameLogic/Commands/Character/SaveCmd.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using ArchaicQuestII.DataAccess;
+using ArchaicQuestII.GameLogic.Account;
+using ArchaicQuestII.GameLogic.Character;
+using ArchaicQuestII.GameLogic.Core;
+using ArchaicQuestII.GameLogic.World.Room;
+
+namespace ArchaicQuestII.GameLogic.Commands.Character
+{
+    public class SaveCmd : ICommand
+    {
+        public SaveCmd(ICore core)
+        {
+            Aliases = new[] {"save"};
+            Description = "Save your character manually";
+            Usages = new[] {"Type: save"};
+            UserRole = UserRole.Player;
+            Core = core;
+        }
+        
+        public string[] Aliases { get; }
+        public string Description { get; }
+        public string[] Usages { get; }
+        public UserRole UserRole { get; }
+        public ICore Core { get; }
+
+        public void Execute(Player player, Room room, string[] input)
+        {
+            Core.PlayerDataBase.Save(player, PlayerDataBase.Collections.Players);
+            Core.Writer.WriteLine("Character saved.", player.ConnectionId);
+        }
+    }
+}
